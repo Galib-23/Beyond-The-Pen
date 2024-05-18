070cb7c083664bcbd1f3681e8cc26e517b91c184
Galib-23 <aagalib2323@gmail.com>
2024-05-18 21:08:57 +0600

    post user fetched

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import styles from "./singlePost.module.css";
+import Image from 'next/image';
+import PostUser from '@/components/postUser/postUser';
 
-const SinglePostPage = () => {
+const getData = async (slug) => {
+  const res = await fetch(`http://localhost:3000/api/blog/${slug}`);
+
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  return res.json();
+};
+
+
+
+const SinglePostPage = async ({params}) => {
+
+  const { slug } = params;
+  const post = await getData(slug);
+
   return (
-    <div>
-      SinglePostPage
+    <div className={styles.container}>
+      {post.img && (
+        <div className={styles.imgContainer}>
+          <Image src={post.img} alt="" fill className={styles.img} />
+        </div>
+      )}
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>{post.title}</h1>
+        <div className={styles.detail}>
+          {post && (
+            <Suspense fallback={<div>Loading...</div>}>
+              <PostUser userId={post.userId} />
+            </Suspense>
+          )}
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>
+              {post.createdAt.toString().slice(4, 16)}
+            </span>
+          </div>
+        </div>
+        <div className={styles.content}>{post.desc}</div>
+      </div>
     </div>
   )
 }
--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import styles from "./blog.module.css";
+import PostCard from '@/components/postCart/postCard';
 
-const Blog = () => {
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/blog", {next:{revalidate:3600}});
+
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  return res.json();
+};
+
+const Blog = async() => {
+
+  const posts = await getData();
+
   return (
     <div className={styles.container}>
-      Blog
+      {posts.map((post) => (
+        <div className={styles.post} key={post.id}>
+          <PostCard post={post} />
+        </div>
+      ))}
     </div>
   )
 }
