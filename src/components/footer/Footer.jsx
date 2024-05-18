4d899de447a2cab583496c678c733eece8ad2be5
Galib-23 <aagalib2323@gmail.com>
2024-05-18 13:54:39 +0600

    contact page and about page created

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className={styles.container}>
+      <div className={styles.logo}>Galib</div>
+      <div className={styles.text}>
+        Asadullah Al Galib © All rights reserved.
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
